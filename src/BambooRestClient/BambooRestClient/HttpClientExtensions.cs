e605d8d2ff8a9dc9f4f34f8dcd644e5198f93cb6
jospehdoolf@gmail.com <jospehdoolf@gmail.com>
2014-07-08 11:08:16 +1000

    created guard clauses and http extensions

---
+++ src/BambooRestClient/BambooRestClient/HttpClientExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BambooRestClient
+{
+    internal static class HttpClientExtensions
+    {
+        public static T[] GetList<T>(this HttpClient client, Link link, Func<dynamic, string> fieldAccessor)
+        {
+            var response = client.GetStringAsync(link.Href);
+            response.Wait();
+
+            dynamic result = JsonConvert.DeserializeObject<JObject>(response.Result);
+
+            return JsonConvert.DeserializeObject<T[]>(fieldAccessor(result));
+        }
+
+        public static  T GetOne<T>(this HttpClient client, Link link)
+        {
+            var response = client.GetStringAsync(link.Href);
+            response.Wait();
+
+            dynamic result = JsonConvert.DeserializeObject<JObject>(response.Result);
+
+            return JsonConvert.DeserializeObject<T>(result.ToString());
+        }
+    }
+}
